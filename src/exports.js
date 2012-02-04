d4b7fc7541e9332f8cea1cb289ae42300deb61bd
ChiperSoft <chiper@chipersoft.com>
2012-02-04 15:07:18 -0800

    Added exports.js in preparation for advanced compiling in GCC

---
+++ src/exports.js
@@ -0,0 +1,12 @@
+window['moment'] = moment;
+
+window['Kalendae'] = Kalendae;
+Kalendae.prototype['getSelectedAsDates'] = Kalendae.prototype.getSelectedAsDates;
+Kalendae.prototype['getSelectedAsText'] = Kalendae.prototype.getSelectedAsText;
+Kalendae.prototype['getSelectedRaw'] = Kalendae.prototype.getSelectedRaw;
+Kalendae.prototype['getSelected'] = Kalendae.prototype.getSelected;
+Kalendae.prototype['isSelected'] = Kalendae.prototype.isSelected;
+Kalendae.prototype['setSelected'] = Kalendae.prototype.setSelected;
+Kalendae.prototype['addSelected'] = Kalendae.prototype.addSelected;
+Kalendae.prototype['removeSelected'] = Kalendae.prototype.removeSelected;
+Kalendae.prototype['draw'] = Kalendae.prototype.draw;
