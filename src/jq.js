447a266346dc569ace0a9c2221d2d849abb60521
crstffr <crstffr@yahoo.com>
2012-03-09 17:46:27 -0600

    The variable "element" was not defined and throwing exceptions, changed to "e" so it would work.

--- src/jq.js
+++ src/jq.js
@@ -1,14 +1,13 @@
 
-
 if (typeof jQuery !== 'undefined') {
 	jQuery.fn.kalendae = function (options) {
 		this.each(function (i, e) {
 			if (e.tagName === 'INPUT') {
 				//if element is an input, bind a popup calendar to the input.
-				$(e).data('kalendae', new Kalendae.Input(element, options));
+				$(e).data('kalendae', new Kalendae.Input(e, options));
 			} else {
 				//otherwise, insert a flat calendar into the element.
-				$(e).data('kalendae', new Kalendae($.extend({}, {attachTo:element}, options)));
+				$(e).data('kalendae', new Kalendae($.extend({}, {attachTo:e}, options)));
 			}
 		});
 		return this;
