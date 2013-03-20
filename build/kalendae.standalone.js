721c6b017d2b04bf07597dcfb3cd6b76f8f18c74
ChiperSoft <chiper@chipersoft.com>
2013-03-20 16:35:07 -0700

    Version number incremented to 0.4

--- build/kalendae.standalone.js
+++ build/kalendae.standalone.js
@@ -2,7 +2,7 @@
  *	Kalendae, a framework agnostic javascript date picker           *
  *	Copyright(c) 2012 Jarvis Badgley (chipersoft@gmail.com)         *
  *	http://github.com/ChiperSoft/Kalendae                           *
- *	Version 0.3                                                     *
+ *	Version 0.4                                                     *
  ********************************************************************/
 
 (function (undefined) {
