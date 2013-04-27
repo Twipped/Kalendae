0b93db1c3ca2ed648399dbe423b49ce1b45e9563
Geremia Taglialatela <tagliala.dev@gmail.com>
2013-04-27 16:45:38 +0200

    Added changelog Bumped version to 0.4.1 Regenerated assets

--- build/kalendae.js
+++ build/kalendae.js
@@ -1,8 +1,8 @@
 /********************************************************************
  *	Kalendae, a framework agnostic javascript date picker           *
- *	Copyright(c) 2012 Jarvis Badgley (chipersoft@gmail.com)         *
+ *	Copyright(c) 2013 Jarvis Badgley (chipersoft@gmail.com)         *
  *	http://github.com/ChiperSoft/Kalendae                           *
- *	Version 0.4                                                     *
+ *	Version 0.4.1                                                   *
  ********************************************************************/
 
 (function (undefined) {
--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 /********************************************************************
  *	Kalendae, a framework agnostic javascript date picker           *
- *	Copyright(c) 2012 Jarvis Badgley (chipersoft@gmail.com)         *
+ *	Copyright(c) 2013 Jarvis Badgley (chipersoft@gmail.com)         *
  *	http://github.com/ChiperSoft/Kalendae                           *
- *	Version 0.4                                                     *
+ *	Version 0.4.1                                                   *
  ********************************************************************/
 
