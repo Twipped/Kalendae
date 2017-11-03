e90ce1099560450c11d7e388f4fee5ba5fe3af6c
Jocelyn Badgley (Twipped) <joc@twipped.com>
2017-11-02 19:22:21 -0700

    Updating author name

--- build/kalendae.js
+++ build/kalendae.js
@@ -1,7 +1,7 @@
 /********************************************************************
  *	Kalendae, a framework agnostic javascript date picker           *
- *	Copyright(c) 2013-2016 Jarvis Badgley (chipersoft@gmail.com)    *
- *	http://github.com/ChiperSoft/Kalendae                           *
+ *	Copyright(c) 2013-2017 Jocelyn Badgley (joc@twipped.com)        *
+ *	http://github.com/Twipped/Kalendae                              *
  *	Version 0.6.1                                                   *
  ********************************************************************/
 
